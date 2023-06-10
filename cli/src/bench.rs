e4290e8cd33a6ed96b776d609bcf38722027cecb
Arron <arronzhang@me.com>
2023-06-10 11:39:56 +0800

    Split lib

---
+++ cli/src/bench.rs
@@ -0,0 +1,125 @@
+use crate::Result;
+use clap::Parser;
+use nostr_db::{Db, Filter, Stats};
+use rayon::prelude::*;
+use std::{
+    path::PathBuf,
+    time::{Duration, Instant},
+};
+
+/// bench options
+#[derive(Debug, Clone, Parser)]
+pub struct BenchOpts {
+    /// Nostr db path
+    #[arg(value_name = "PATH")]
+    pub path: PathBuf,
+
+    /// [NIP-01](https://nips.be/1) Filter
+    #[arg(short = 'f', long, value_name = "FILTER", default_value = "{}")]
+    pub filter: Filter,
+
+    /// only bench the count method
+    #[arg(long, value_name = "BOOL")]
+    pub count: bool,
+}
+
+pub fn bench_opts(mut opts: BenchOpts) -> anyhow::Result<u64> {
+    opts.filter.build_words();
+    let count = bench(&opts.path, &opts.filter, opts.count)?;
+    Ok(count)
+}
+
+pub fn bench(path: &PathBuf, filter: &Filter, count: bool) -> Result<u64> {
+    fn once(db: &Db, filter: &Filter, count: bool) -> Result<(u64, Stats)> {
+        let reader = db.reader()?;
+        let mut iter = db.iter::<String, _>(&reader, &filter)?;
+        if count {
+            Ok(iter.size()?)
+        } else {
+            let mut c = 0;
+            while let Some(event) = iter.next() {
+                let _json: String = event?;
+                c += 1;
+            }
+            Ok((c, iter.stats()))
+        }
+    }
+
+    let db = Db::open(path)?;
+    let now = Instant::now();
+    let res = once(&db, &filter, count)?;
+    let elapsed = now.elapsed();
+
+    println!("{:?}", filter);
+    println!("Size: {:?}", res.0);
+    println!("{:?}", res.1);
+    println!("Time: {:?}, {}", elapsed, fmt_per_sec(1, &elapsed));
+    let mut times = (Duration::from_secs(2).as_nanos() / elapsed.as_nanos()) as u64;
+    if times == 0 {
+        times = 10;
+    }
+
+    println!("Bench prepare");
+    let now = Instant::now();
+    for _i in 0..times {
+        let _r = once(&db, &filter, count)?;
+    }
+    let elapsed = now.elapsed();
+    println!(
+        "Time: {:?}, {}",
+        elapsed / times as u32,
+        fmt_per_sec(times, &elapsed)
+    );
+
+    println!("Bench single threaded");
+    // start bench
+    let mut times = Duration::from_secs(5).as_nanos() as u64 * times / elapsed.as_nanos() as u64;
+    if times == 0 {
+        times = 10;
+    }
+
+    let now = Instant::now();
+    for _i in 0..times {
+        let _r = once(&db, &filter, count)?;
+    }
+    let elapsed = now.elapsed();
+    println!(
+        "Time: {:?}, {}",
+        elapsed / times as u32,
+        fmt_per_sec(times, &elapsed)
+    );
+
+    println!("Bench multi-threaded");
+    let now = Instant::now();
+    (0..times).into_par_iter().for_each(|_| {
+        let _r = once(&db, &filter, count);
+        if let Err(e) = _r {
+            println!("{:?}", e);
+        }
+    });
+    let elapsed = now.elapsed();
+    println!(
+        "Time: {:?}, {}",
+        elapsed / times as u32,
+        fmt_per_sec(times, &elapsed)
+    );
+    Ok(res.0)
+}
+
+pub fn fmt_num(count: f64) -> String {
+    let ret = if count < 1_000.0 {
+        format!("{:.1}", count)
+    } else if count < 1_000_000.0 {
+        format!("{:.1}K", count / 1_000.0)
+    } else if count < 1_000_000_000.0 {
+        format!("{:.1}M", count / 1_000_000.0)
+    } else {
+        format!("{:.1}G", count / 1_000_000_000.0)
+    };
+    ret
+}
+
+pub fn fmt_per_sec(count: u64, dur: &Duration) -> String {
+    let count = (count as f64) / (dur.as_nanos() as f64) * 1_000_000_000.0;
+    format!("{}/s", fmt_num(count))
+}
--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,15 +1,16 @@
 use clap::Parser;
 use clio::{Input, Output};
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
-use nostr_db::{Db, Event, Filter, FromEventData, Stats};
+use nostr_db::{Db, Event, Filter, FromEventData};
 use rayon::prelude::*;
 use std::{
     fs::File,
     io::{BufRead, BufReader, Write},
     path::{Path, PathBuf},
-    println,
-    time::{Duration, Instant},
 };
+
+mod bench;
+pub use bench::*;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -25,7 +26,7 @@ pub enum Error {
     Message(String),
 }
 
-type Result<T, E = Error> = core::result::Result<T, E>;
+pub type Result<T, E = Error> = core::result::Result<T, E>;
 
 /// import options
 #[derive(Debug, Clone, Parser)]
@@ -61,22 +62,6 @@ pub struct ExportOpts {
     /// output jsonl data file, use '-' for stdout
     #[clap(value_parser, default_value = "-")]
     pub output: Output,
-}
-
-/// bench options
-#[derive(Debug, Clone, Parser)]
-pub struct BenchOpts {
-    /// Nostr db path
-    #[arg(value_name = "PATH")]
-    pub path: PathBuf,
-
-    /// [NIP-01](https://nips.be/1) Filter
-    #[arg(short = 'f', long, value_name = "FILTER", default_value = "{}")]
-    pub filter: Filter,
-
-    /// only bench the count method
-    #[arg(long, value_name = "BOOL")]
-    pub count: bool,
 }
 
 /// import
@@ -243,105 +228,4 @@ pub fn export<F: Fn(usize)>(
     }
     output.finish()?;
     Ok(count)
-}
-
-pub fn bench_opts(mut opts: BenchOpts) -> anyhow::Result<u64> {
-    opts.filter.build_words();
-    let count = bench(&opts.path, &opts.filter, opts.count)?;
-    Ok(count)
-}
-
-pub fn bench(path: &PathBuf, filter: &Filter, count: bool) -> Result<u64> {
-    fn once(db: &Db, filter: &Filter, count: bool) -> Result<(u64, Stats)> {
-        let reader = db.reader()?;
-        let mut iter = db.iter::<String, _>(&reader, &filter)?;
-        if count {
-            Ok(iter.size()?)
-        } else {
-            let mut c = 0;
-            while let Some(event) = iter.next() {
-                let _json: String = event?;
-                c += 1;
-            }
-            Ok((c, iter.stats()))
-        }
-    }
-
-    let db = Db::open(path)?;
-    let now = Instant::now();
-    let res = once(&db, &filter, count)?;
-    let elapsed = now.elapsed();
-
-    println!("{:?}", filter);
-    println!("Size: {:?}", res.0);
-    println!("{:?}", res.1);
-    println!("Time: {:?}, {}", elapsed, fmt_per_sec(1, &elapsed));
-    let mut times = (Duration::from_secs(2).as_nanos() / elapsed.as_nanos()) as u64;
-    if times == 0 {
-        times = 10;
-    }
-
-    println!("Bench prepare");
-    let now = Instant::now();
-    for _i in 0..times {
-        let _r = once(&db, &filter, count)?;
-    }
-    let elapsed = now.elapsed();
-    println!(
-        "Time: {:?}, {}",
-        elapsed / times as u32,
-        fmt_per_sec(times, &elapsed)
-    );
-
-    println!("Bench single threaded");
-    // start bench
-    let mut times = Duration::from_secs(5).as_nanos() as u64 * times / elapsed.as_nanos() as u64;
-    if times == 0 {
-        times = 10;
-    }
-
-    let now = Instant::now();
-    for _i in 0..times {
-        let _r = once(&db, &filter, count)?;
-    }
-    let elapsed = now.elapsed();
-    println!(
-        "Time: {:?}, {}",
-        elapsed / times as u32,
-        fmt_per_sec(times, &elapsed)
-    );
-
-    println!("Bench multi-threaded");
-    let now = Instant::now();
-    (0..times).into_par_iter().for_each(|_| {
-        let _r = once(&db, &filter, count);
-        if let Err(e) = _r {
-            println!("{:?}", e);
-        }
-    });
-    let elapsed = now.elapsed();
-    println!(
-        "Time: {:?}, {}",
-        elapsed / times as u32,
-        fmt_per_sec(times, &elapsed)
-    );
-    Ok(res.0)
-}
-
-pub fn fmt_num(count: f64) -> String {
-    let ret = if count < 1_000.0 {
-        format!("{:.1}", count)
-    } else if count < 1_000_000.0 {
-        format!("{:.1}K", count / 1_000.0)
-    } else if count < 1_000_000_000.0 {
-        format!("{:.1}M", count / 1_000_000.0)
-    } else {
-        format!("{:.1}G", count / 1_000_000_000.0)
-    };
-    ret
-}
-
-pub fn fmt_per_sec(count: u64, dur: &Duration) -> String {
-    let count = (count as f64) / (dur.as_nanos() as f64) * 1_000_000_000.0;
-    format!("{}/s", fmt_num(count))
 }
