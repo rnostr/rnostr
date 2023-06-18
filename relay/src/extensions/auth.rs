9a9ec769ead85398c28ce012b3e34cce8d1418f7
Arron <arronzhang@me.com>
2023-06-18 12:52:32 +0800

    Test auth

--- relay/src/extensions/auth.rs
+++ relay/src/extensions/auth.rs
@@ -140,7 +140,7 @@ impl Extension for Auth {
                     if let Some(AuthState::Challenge(challenge)) = state {
                         if let Err(err) = event.validate(now(), 0, 0) {
                             return OutgoingMessage::notice(&err.to_string()).into();
-                        } else {
+                        } else if event.kind() == 22242 {
                             for tag in event.tags() {
                                 if tag.len() > 1 && tag[0] == "challenge" && &tag[1] == challenge {
                                     session.set(AuthState::Pubkey(event.pubkey_str()));
@@ -176,5 +176,216 @@ impl Extension for Auth {
             }
         }
         ExtensionMessageResult::Continue(msg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{create_test_app, create_web_app};
+    use actix_web::web;
+    use actix_web_actors::ws;
+    use anyhow::Result;
+    use futures_util::{SinkExt as _, StreamExt as _};
+    use nostr_db::{
+        secp256k1::{rand::thread_rng, KeyPair, XOnlyPublicKey},
+        Event,
+    };
+
+    fn parse_text<T: serde::de::DeserializeOwned>(frame: &ws::Frame) -> Result<T> {
+        if let ws::Frame::Text(text) = &frame {
+            let data: T = serde_json::from_slice(text)?;
+            Ok(data)
+        } else {
+            Err(crate::Error::Message("invalid frame type".to_string()).into())
+        }
+    }
+
+    #[actix_rt::test]
+    async fn auth() -> Result<()> {
+        let mut rng = thread_rng();
+        let key_pair = KeyPair::new_global(&mut rng);
+
+        let app = create_test_app("auth")?;
+        {
+            let mut w = app.setting.write();
+            w.extra = serde_json::from_str(
+                r#"{
+                "auth": {
+                    "enabled": true
+                }
+            }"#,
+            )?;
+        }
+        let app = app.add_extension(Auth::new());
+        let app = web::Data::new(app);
+
+        let mut srv = actix_test::start(move || create_web_app(app.clone()));
+
+        // client service
+        let mut framed = srv.ws_at("/").await.unwrap();
+
+        let item = framed.next().await.unwrap()?;
+        assert!(matches!(item, ws::Frame::Text(_)));
+        let state: (String, String) = parse_text(&item)?;
+        assert_eq!(state.0, "AUTH");
+
+        let event = Event::create(&key_pair, 0, 1, vec![], "".to_owned())?;
+        let event = Event::new(
+            event.id().to_vec(),
+            event.pubkey().to_vec(),
+            event.created_at(),
+            2,
+            vec![],
+            "".to_owned(),
+            event.sig().to_vec(),
+        )?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["AUTH", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.1.contains("invalid"));
+
+        let event = Event::create(&key_pair, now(), 22242, vec![], "".to_owned())?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["AUTH", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.1.contains("error"));
+
+        let event = Event::create(
+            &key_pair,
+            now(),
+            22242,
+            vec![vec!["challenge".to_owned(), state.1.clone()]],
+            "".to_owned(),
+        )?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["AUTH", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.1.contains("success"));
+
+        framed
+            .send(ws::Message::Close(Some(ws::CloseCode::Normal.into())))
+            .await?;
+        let item = framed.next().await.unwrap()?;
+        assert_eq!(item, ws::Frame::Close(Some(ws::CloseCode::Normal.into())));
+        Ok(())
+    }
+
+    #[actix_rt::test]
+    async fn pubkey_whitelist() -> Result<()> {
+        let mut rng = thread_rng();
+        let key_pair = KeyPair::new_global(&mut rng);
+        let pubkey = XOnlyPublicKey::from_keypair(&key_pair).0;
+
+        let app = create_test_app("auth-whitelist")?;
+        {
+            let mut w = app.setting.write();
+            w.extra = serde_json::from_str(&format!(
+                r#"{{
+                "auth": {{
+                    "enabled": true,
+                    "write": {{
+                        "pubkey_whitelist": ["{}"]
+                    }}
+                }}
+            }}"#,
+                pubkey.to_string()
+            ))?;
+        }
+        let app = app.add_extension(Auth::new());
+        let app = web::Data::new(app);
+
+        let mut srv = actix_test::start(move || create_web_app(app.clone()));
+
+        // client service
+        let mut framed = srv.ws_at("/").await.unwrap();
+
+        let item = framed.next().await.unwrap()?;
+        assert!(matches!(item, ws::Frame::Text(_)));
+        let state: (String, String) = parse_text(&item)?;
+        assert_eq!(state.0, "AUTH");
+
+        let event = Event::create(
+            &key_pair,
+            now(),
+            22242,
+            vec![vec!["challenge".to_owned(), state.1.clone()]],
+            "".to_owned(),
+        )?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["AUTH", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.1.contains("success"));
+
+        // write
+        let event = Event::create(&key_pair, now(), 1, vec![], "test".to_owned())?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["EVENT", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String, bool, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.2);
+
+        framed
+            .send(ws::Message::Close(Some(ws::CloseCode::Normal.into())))
+            .await?;
+        let item = framed.next().await.unwrap()?;
+        assert_eq!(item, ws::Frame::Close(Some(ws::CloseCode::Normal.into())));
+
+        let key_pair1 = KeyPair::new_global(&mut rng);
+        // client service
+        let mut framed = srv.ws_at("/").await.unwrap();
+
+        let item = framed.next().await.unwrap()?;
+        assert!(matches!(item, ws::Frame::Text(_)));
+        let state: (String, String) = parse_text(&item)?;
+        assert_eq!(state.0, "AUTH");
+
+        let event = Event::create(
+            &key_pair1,
+            now(),
+            22242,
+            vec![vec!["challenge".to_owned(), state.1.clone()]],
+            "".to_owned(),
+        )?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["AUTH", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.1.contains("success"));
+
+        // write
+        let event = Event::create(&key_pair, now(), 1, vec![], "test".to_owned())?;
+        framed
+            .send(ws::Message::Text(
+                format!(r#"["EVENT", {}]"#, event.to_string()).into(),
+            ))
+            .await?;
+        let notice: (String, String, bool, String) = parse_text(&framed.next().await.unwrap()?)?;
+        assert!(notice.3.contains("restricted"));
+        assert!(!notice.2);
+
+        framed
+            .send(ws::Message::Close(Some(ws::CloseCode::Normal.into())))
+            .await?;
+        let item = framed.next().await.unwrap()?;
+        assert_eq!(item, ws::Frame::Close(Some(ws::CloseCode::Normal.into())));
+
+        Ok(())
     }
 }
